71a1abf099fef4e1c6a021399adb8e8f69dc51a1
Ashutosh-tec <ashu@Ashu-Desktop.localdomain>
2022-12-16 23:47:09 +0530

    Add existing file

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template 
-from models import db, User as User, List as List, Todo as Todo
+from models import db, User as User, List as List, Task as Task
+from api.resource import api, Users
 
 
 
@@ -9,11 +10,11 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo_database.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
-
+api.init_app(app)
 
 # @app.before_first_request
 # def create_db():
-    # db.create_all()
+#     db.create_all()
     # usr= User(user_id="001", user_password="001")
     # db.session.add(usr)
     # db.session.commit()
--- models.py
+++ models.py
@@ -1,39 +1,40 @@
 # from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-# app = Flask(__name__)
+
 
 db = SQLAlchemy()
 class User(db.Model):
     __tablename__ =  'user'
-    user_id = db.Column(db.String, primary_key=True)
+    user_id = db.Column(db.Integer, primary_key=True)  #should store locally to get whenever needed
+    user_name = db.Column(db.String, unique=True, nullable=False)  #needed for log in
+    user_email = db.Column(db.String, nullable=False)
     user_password = db.Column(db.String, nullable=False)
-    # user = db.relationship("Todo", backref= "user")
     lists = db.relationship("List", backref= "user")
     def __repr__(self):
         return "<User's ID is %r>" % self.user_id
 
 class List(db.Model):
     __tablename__ = 'list'
-    list_id = db.Column(db.Integer, autoincrement=True, primary_key = True)
+    list_id = db.Column(db.Integer, primary_key = True)
     list_name = db.Column(db.String, nullable=False)
     list_description = db.Column(db.String)
     user_id = db.Column(db.String,db.ForeignKey("user.user_id"), nullable=False)
-    tasks = db.relationship("Todo", backref= "list")
+    tasks = db.relationship("Task", backref= "list")
     
     # list = db.relationship("Todo", backref="list")
     def __repr__(self):
         return "<List's ID is %r>" % self.list_id
 
 
-class Todo(db.Model):
-    __tablename__ = 'todo'
-    todo_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    todo_title = db.Column(db.String, nullable = False)
-    todo_content = db.Column(db.String, nullable = False)
-    todo_deadline = db.Column(db.String(10), nullable = False)
-    todo_created_time = db.Column(db.String(10), nullable = False)
-    todo_completed_time = db.Column(db.String(10), nullable = True)
-    todo_status = db.Column(db.String, nullable = False)  # if task is complete "Completed" else "Pending"
+class Task(db.Model):
+    __tablename__ = 'task'
+    task_id = db.Column(db.Integer, primary_key=True)
+    task_title = db.Column(db.String, nullable = False)
+    task_content = db.Column(db.String, nullable = False)
+    task_deadline = db.Column(db.String(10), nullable = False)
+    task_created_time = db.Column(db.String(10), nullable = False)
+    task_completed_time = db.Column(db.String(10), nullable = True)
+    task_status = db.Column(db.String, nullable = False)  # if task is complete "Completed" else "Pending"
     list_id = db.Column(db.String,db.ForeignKey("list.list_id"), nullable=False)
 
 
